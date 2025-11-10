e64103fe860057a2117685b81ad452e1a9f2e1b5
Eudivan de Melo <eudivan44@gmail.com>
2025-11-10 17:15:19 -0300

    initilized project

---
+++ src/main/java/br/edu/ifrn/polling_and_voting/PollingAndVotingApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.polling_and_voting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingAndVotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollingAndVotingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifrn/polling_and_voting/PollingAndVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.polling_and_voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingAndVotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
